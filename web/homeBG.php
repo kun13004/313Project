304e00ec39b6ea949023f667691011b0cf169c17
Austin Kunz <kun13004@byui.edu>
2016-10-15 20:43:33 -0600

    s

--- web/homeBG.php
+++ web/homeBG.php
@@ -32,7 +32,7 @@
 		$term = $_POST["search"];
 
 		echo "$term";
-		$result = $db->prepare('SELECT * FROM member WHERE name = "$term"');
+		$result = $db->prepare('SELECT * FROM member WHERE first_name = "$term"');
 		$result->execute();
 
 		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
