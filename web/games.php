1c5ace2ee3514e991a8f5ef638a78a91c650fdde
Austin Kunz <kun13004@byui.edu>
2016-10-17 17:49:53 -0600

    s

--- web/games.php
+++ web/games.php
@@ -27,9 +27,6 @@
  			print "<p>error: $ex->getMessage() </p>\n\n";
  			die();
 		}
-		$term = pg_escape_string($_REQUEST['game_title']);
-		echo $term;
-		echo $_POST['game_title'];
 
 		$result = $db->prepare("SELECT * FROM game");
 		$result->execute();
