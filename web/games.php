f2b5fafe399c62462c81eb029b2cd3e2cdea9c46
Austin Kunz <kun13004@byui.edu>
2016-10-17 14:01:09 -0600

    s

--- web/games.php
+++ web/games.php
@@ -6,11 +6,9 @@
 <body>
 	<h1>List of games</h1>
 	<?php
-		// default Heroku Postgres configuration URL
 		$dbUrl = getenv('DATABASE_URL');
 
 		if (empty($dbUrl)) {
- 		// example localhost configuration URL with postgres username and a database called cs313db
  		$dbUrl = "postgres://hugtqfrjvkgjma:7dj1BOGitBNwtoO_b0dJzI9Jfg@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d1ci1fmm9irifj";
 		}
 
@@ -24,19 +22,18 @@
 
 		try {
 			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		}
+
+			$result = $db->prepare('SELECT * FROM game');
+			$result->execute();
+			while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+				echo $row['game_title'] . '<br>';
+				echo $row['game_description'] . '<br>';
+				echo "<br />\n";
+			}
 		}
 		catch (PDOException $ex) {
  			print "<p>error: $ex->getMessage() </p>\n\n";
  			die();
-		}
-
-		$result = $db->prepare("SELECT * FROM game");
-		$result->execute();
-		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			echo $row['game_title'] . '<br>';
-			echo $row['game_description'] . '<br>';
-			echo "<br />\n";
 		}
 	?>
 </body>
