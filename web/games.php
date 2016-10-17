ba6d82db1c85e7ff41e48f495601686315f2a80f
Austin Kunz <kun13004@byui.edu>
2016-10-17 13:24:58 -0600

    adding pages for site

---
+++ web/games.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Game List</title>
+</head>
+<body>
+	<h1>List of games</h1>
+	<?php
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+
+		if (empty($dbUrl)) {
+ 		// example localhost configuration URL with postgres username and a database called cs313db
+ 		$dbUrl = "postgres://postgres:password@localhost:5432/scriptures";
+		}
+
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"]; 
+ 		$dbPort = $dbopts["port"]; 
+ 		$dbUser = $dbopts["user"]; 
+ 		$dbPassword = $dbopts["pass"];
+ 		$dbName = ltrim($dbopts["path"],'/');
+
+		try {
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		}
+		}
+		catch (PDOException $ex) {
+ 			print "<p>error: $ex->getMessage() </p>\n\n";
+ 			die();
+		}
+		//$term = mysql_real_escape_string($_REQUEST['forums']); 
+		$term = pg_escape_string($_REQUEST['search']);
+
+		$result = $db->prepare("SELECT * FROM game WHERE game_title LIKE '%$term%' OR game_subtitle LIKE '%$term%' OR game_description LIKE '%$term%'");
+		$result->execute();
+		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+			echo $row['game_title'] . '<br>';
+			echo "<br />\n";
+		}
+
+
+	?>
+</body>
+</html>
