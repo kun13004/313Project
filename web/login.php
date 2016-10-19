d748617861c5c071f7c2440efb762aaba97bb914
Austin Kunz <kun13004@byui.edu>
2016-10-19 16:11:48 -0600

    Create new user

--- web/login.php
+++ web/login.php
@@ -1,1 +1,54 @@
-login.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		$dbUrl = getenv('DATABASE_URL');
+
+		if (empty($dbUrl)) {
+ 		$dbUrl = "postgres://hugtqfrjvkgjma:7dj1BOGitBNwtoO_b0dJzI9Jfg@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d1ci1fmm9irifj";
+		}
+
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"]; 
+ 		$dbPort = $dbopts["port"]; 
+ 		$dbUser = $dbopts["user"]; 
+ 		$dbPassword = $dbopts["pass"];
+ 		$dbName = ltrim($dbopts["path"],'/');
+
+		try {
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		}
+		catch (PDOException $ex) {
+ 			print "<p>error: $ex->getMessage() </p>\n\n";
+ 			die();
+		}
+
+		$first_name = filter_input(INPUT_POST, 'firstname');
+		$last_name = filter_input(INPUT_POST, 'lastname');
+		$country_code = filter_input(INPUT_POST, 'country');
+		$area_code = filter_input(INPUT_POST, 'area');
+		$phone_number = filter_input(INPUT_POST, 'phone');
+		$phone_type = filter_input(INPUT_POST, 'type');
+		$email = filter_input(INPUT_POST, 'email');
+		$user_name = filter_input(INPUT_POST, 'user_name');
+		$password = filter_input(INPUT_POST, 'password');
+
+
+
+		$query = "INSERT INTO member (user_name, password, first_name, last_name, email) VALUES ('$user_name', '$password', '$first_name', '$last_name', '$email')";
+
+		$result = pg_query($query);
+            if (!$result) { 
+            $errormessage = pg_last_error(); 
+            echo "Error with query: " . $errormessage; 
+            exit(); 
+            } 
+            printf ("These values were inserted into the database - %s %s %s"); 
+            pg_close();
+	?>
+</body>
+</html>
