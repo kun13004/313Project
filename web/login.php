21dffc75bb07936a43fe268161bf5b9beebc2621
Austin Kunz <kun13004@byui.edu>
2016-10-19 17:08:50 -0600

    s

--- web/login.php
+++ web/login.php
@@ -19,13 +19,9 @@
  		$dbPassword = $dbopts["pass"];
  		$dbName = ltrim($dbopts["path"],'/');
 
-		try {
-			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		}
-		catch (PDOException $ex) {
- 			print "<p>error: $ex->getMessage() </p>\n\n";
- 			die();
-		}
+		
+		$db = pg_connect('host=$dbHost dbname=$dbName user=$dbUser password=$dbPassword');
+		
 
 		$first_name = pg_escape_string($_POST['firstname']);
 		$last_name = pg_escape_string($_POST['lastname']);
