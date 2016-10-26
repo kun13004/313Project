6f925bca2d96587d03fdc4ecc82d3d2616e57c95
Austin Kunz <kun13004@byui.edu>
2016-10-26 14:47:53 -0600

    s

--- web/profile.php
+++ web/profile.php
@@ -42,7 +42,7 @@ session_start();
 
 		pg_close();
 
-		header("Location: https://fathomless-plateau-18398.herokuapp.com/BoardGameHome.html");
+		header("Location: https://fathomless-plateau-18398.herokuapp.com/BoardGameHome.php");
 	?>
 </body>
 </html>
