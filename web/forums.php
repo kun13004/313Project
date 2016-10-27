6c90aa1420cf1dccfc6e228468a6a8360ed8e7c2
Austin Kunz <kun13004@byui.edu>
2016-10-27 17:04:58 -0600

    s

--- web/forums.php
+++ web/forums.php
@@ -8,14 +8,36 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div>
-	<a href="BoardGameHome.php">Home</a>
-	<?php
-		if ($_SESSION["username"] != "") {
-      		echo "<p>Welcome " . $_SESSION["username"] . "<p><br>";
-    	}
-    ?>
-	</div>
+	
+	<!-- Header -->
+  <ul>
+    <li style="float:right;">
+      <form method="post" action="search.php">
+        <input type="text" name="search" placeholder="Search..">
+      </form>
+    </li>
+  <?php
+    $_SESSION["username"];
+    $_SESSION["ftopic"];
+    $log = $_POST["name"];
+    if ($log == "logout") {
+      $_SESSION["username"] = "";
+    }
+    if ($_SESSION["username"] != "") {
+      echo '<li style="float:right"><p>Welcome ' . $_SESSION["username"] . '</p></li>';
+      echo '<li style="float:right"><a href="logout.php">Log out</a></li>';
+    }
+    else {
+      echo '<li style="float:right"><a href="login.html">Log in</a></li>';
+      echo '<li style="float:right"><a href="signup.html">Sign up</a></li>';
+    }
+  ?>
+      <li><a href="BoardGameHome.php">Home</a></li>
+  		<li><a href="games.php">Games</a></li>
+  		<li><a class="active" href="forums.php">Forums</a></li>
+  		<li><a href="about.php">About</a></li>
+	 </ul>
+	
 	<h1>List Forums</h1>
 	<?php
 		// default Heroku Postgres configuration URL
