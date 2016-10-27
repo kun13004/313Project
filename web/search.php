9559357c87e0617a406d4d62ca300c28bad4c221
Austin Kunz <kun13004@byui.edu>
2016-10-26 22:32:28 -0600

    s

--- web/search.php
+++ web/search.php
@@ -20,8 +20,22 @@ session_start();
 		ini_set('display_errors','on');
         error_reporting(E_ALL);
 
+		$dbUrl = getenv('DATABASE_URL');
+
+		if (empty($dbUrl)) {
+ 		$dbUrl = "postgres://hugtqfrjvkgjma:7dj1BOGitBNwtoO_b0dJzI9Jfg@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d1ci1fmm9irifj";
+		}
+
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"]; 
+ 		$dbPort = $dbopts["port"]; 
+ 		$dbUser = $dbopts["user"]; 
+ 		$dbPassword = $dbopts["pass"];
+ 		$dbName = ltrim($dbopts["path"],'/');
+
 		try {
-			$db = pg_connect('host=ec2-54-243-54-21.compute-1.amazonaws.com dbname=d1ci1fmm9irifj user=hugtqfrjvkgjma password=7dj1BOGitBNwtoO_b0dJzI9Jfg');
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		}
 		catch (PDOException $ex) {
  			print "<p>error: $ex->getMessage() </p>\n\n";
