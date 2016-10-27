422e1cb2f90a6b2ee170e27dcd82af1b7529bd98
Austin Kunz <kun13004@byui.edu>
2016-10-26 22:47:02 -0600

    s

--- web/addGame.php
+++ web/addGame.php
@@ -9,10 +9,10 @@
 		<strong>Title</strong>
 		<input type="text" name="title"><br><br>
 		<strong>Subtitle</strong>
-		<input type="text" name="subtitle"><br><br>
-		<strong>Barcode</strong>
 		<input type="text" name="image"><br><br>
 		<strong>Image Url</strong>
+		<input type="text" name="subtitle"><br><br>
+		<strong>Barcode</strong>
 		<input type="text" name="barcode"> example (BDGM*****) where * = int<br><br>
 		<input type="radio" name="bgtype" value="1" checked> Dexterity<br>
   		<input type="radio" name="bgtype" value="2"> Bluffing<br>
