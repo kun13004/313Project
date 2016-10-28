27fa308a4b159bdde6e3372db49cf6484b37e77d
Austin Kunz <kun13004@byui.edu>
2016-10-27 18:54:52 -0600

    s

--- web/BoardGameHome.php
+++ web/BoardGameHome.php
@@ -25,8 +25,27 @@ session_start();
 </head>
 <body>
 
-  <!-- Header -->
-  <ul>
+  
+
+   
+
+
+
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <!-- Header -->
+  
     <li style="float:right;">
       <form method="post" action="search.php">
         <input type="text" name="search" placeholder=" Search..">
@@ -49,12 +68,31 @@ session_start();
     }
   ?>
       <li><a class="active" href="#">Home</a></li>
-  		<li><a href="games.php">Games</a></li>
-  		<li><a href="forums.php">Forums</a></li>
-  		<li><a href="about.php">About</a></li>
-	 </ul>
+      <li><a href="games.php">Games</a></li>
+      <li><a href="forums.php">Forums</a></li>
+      <li><a href="about.php">About</a></li>
+   
 
-   <div class="container">
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container-fluid text-center">
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="col-sm-8 text-left">
+      
+    <!-- Put the carousel here -->
+    <div class="container">
   <br>
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
@@ -95,6 +133,29 @@ session_start();
     </a>
   </div>
 </div>
+
+
+
+    </div>
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+
+
+
+
+
   
 </body>
 </html>
