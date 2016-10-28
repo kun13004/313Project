7dd0abeb1b46a9e66975047651044e568f9789a7
Austin Kunz <kun13004@byui.edu>
2016-10-27 21:34:15 -0600

    d

--- web/games.php
+++ web/games.php
@@ -72,7 +72,7 @@ session_start();
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
     </div>
-    <div class="col-sm-8 text-left">
+    <div class="col-sm-8 text-left gray">
 
     	<!-- List the Games -->
     	<h1>List of games</h1>
