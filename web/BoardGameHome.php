84b4eb01733eb82c046144cb26ef90c8b7aca8f2
Austin Kunz <kun13004@byui.edu>
2016-10-27 19:18:55 -0600

    d

--- web/BoardGameHome.php
+++ web/BoardGameHome.php
@@ -33,7 +33,7 @@ session_start();
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="#"><img style="width:25px;height: 25px;" src="http://sweaglesw.org/dicewizard/dice-icon.png"></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
