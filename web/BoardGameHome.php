75812b5ea0bbab86fba874ce54fa587a695f0cfd
Austin Kunz <kun13004@byui.edu>
2016-10-27 19:15:44 -0600

    d

--- web/BoardGameHome.php
+++ web/BoardGameHome.php
@@ -14,7 +14,7 @@ session_start();
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+  <link rel="stylesheet" type="text/css" href="style.css">
   <style>
   .carousel-inner > .item > img,
   .carousel-inner > .item > a > img {
@@ -75,7 +75,7 @@ session_start();
   
 <div class="container-fluid text-center">
   <div class="row content">
-    <div class="col-sm-2 sidenav">
+    <div class="col-sm-1 sidenav">
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
@@ -124,6 +124,7 @@ session_start();
         </a>
       </div>
     </div>
+    <br>
 
 
 <!--    <div class="col-sm-2 sidenav">
