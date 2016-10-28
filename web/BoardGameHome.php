d2603ffae8b30432f5f3385d9c903180662de433
Austin Kunz <kun13004@byui.edu>
2016-10-27 19:12:59 -0600

    d

--- web/BoardGameHome.php
+++ web/BoardGameHome.php
@@ -84,56 +84,57 @@ session_start();
       
     <!-- Put the carousel here -->
     <div class="container">
-    <br>
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-      </ol>
+      <br>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="http://gamingtrend.com/wp-content/screenshots/black-fleet/img_0674.jpg" alt="Chania" width="460" height="345">
-        </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="http://gamingtrend.com/wp-content/screenshots/black-fleet/img_0674.jpg" alt="Chania" width="460" height="345">
+          </div>
 
-        <div class="item">
-          <img src="https://www.shutupandsitdown.com/wp-content/uploads/2011/08/75654478f75011e2b73af23c91709c91_1374993571.jpg" alt="Chania" width="460" height="345">
-        </div>
+          <div class="item">
+            <img src="https://www.shutupandsitdown.com/wp-content/uploads/2011/08/75654478f75011e2b73af23c91709c91_1374993571.jpg" alt="Chania" width="460" height="345">
+          </div>
     
-        <div class="item">
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/s--fOtZ-iNu--/c_scale,fl_progressive,q_80,w_800/1464500061578344995.jpg" alt="Flower" width="460" height="345">
+          <div class="item">
+            <img src="https://i.kinja-img.com/gawker-media/image/upload/s--fOtZ-iNu--/c_scale,fl_progressive,q_80,w_800/1464500061578344995.jpg" alt="Flower" width="460" height="345">
+          </div>
+
+          <div class="item">
+            <img src="http://www.boardgamequest.com/wp-content/uploads/2015/10/Pandemic-Legacy-Header.jpg" alt="Flower" width="460" height="345">
+          </div>
         </div>
 
-        <div class="item">
-          <img src="http://www.boardgamequest.com/wp-content/uploads/2015/10/Pandemic-Legacy-Header.jpg" alt="Flower" width="460" height="345">
-        </div>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
-  </div>
 
 
-  <div class="col-sm-2 sidenav">
-    <div class="well">
-      <p>ADS</p>
+<!--    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
     </div>
-    <div class="well">
-      <p>ADS</p>
-    </div>
-  </div>
+  </div>  -->
 </div>
 
 <footer class="container-fluid text-center dark-gray">
